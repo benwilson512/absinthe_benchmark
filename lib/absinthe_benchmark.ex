af91eb7f384429191c52f24a1c58257ae30b2a27
Sebastian Kosch <sebastian@aldusleaf.org>
2017-02-15 17:11:41 -0500

    Initial commit

---
+++ lib/absinthe_benchmark.ex
@@ -0,0 +1,18 @@
+defmodule AbsintheBenchmark do
+  @moduledoc """
+  Documentation for AbsintheBenchmark.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AbsintheBenchmark.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/absinthe_benchmark_test.exs
@@ -0,0 +1,8 @@
+defmodule AbsintheBenchmarkTest do
+  use ExUnit.Case
+  doctest AbsintheBenchmark
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
